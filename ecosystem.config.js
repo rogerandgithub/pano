35895e24e54dcec2192682dd09eaf53737cee38a
roger <280135694@qq.com>
2018-12-13 13:30:45 +0800

    update pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,40 +10,16 @@ module.exports = {
             script: './bin/www',
             // watch: ['.well-known', 'bin', 'config', 'https_cert', 'node_modules', 'public', 'routes', 'views'],
             // ignore_watch: ['routes/newitem/*.jpg', 'views'],
-            autorestart: true,
-            max_memory_restart: '1G',
-            env: {
-                COMMON_VARIABLE: 'true'
-            },
-            env_production: {
-                NODE_ENV: 'production'
-            }
+            'instances': 'max',
+            'exec_mode': 'cluster',
+            log_date_format: 'YYYY-MM-DD HH:mm:ss',
+            error_file: 'pm2_err_log/pm2_err.log',
+            //out_file: 'pm2_out_log/pm2_out.log',
         }
-    ],
+    ]
 
     /**
      * Deployment section
      * http://pm2.keymetrics.io/docs/usage/deployment/
      */
-    deploy: {
-        production: {
-            user: 'node',
-            host: '212.83.163.1',
-            ref: 'origin/master',
-            repo: 'git@github.com:repo.git',
-            path: '/var/www/production',
-            'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production'
-        },
-        dev: {
-            user: 'node',
-            host: '212.83.163.1',
-            ref: 'origin/master',
-            repo: 'git@github.com:repo.git',
-            path: '/var/www/development',
-            'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env dev',
-            env: {
-                NODE_ENV: 'dev'
-            }
-        }
-    }
 };
