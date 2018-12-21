30e61d0e75a6e1d95169ed38bea4239b7e193448
roger <280135694@qq.com>
2018-12-21 14:39:49 +0800

    update latest test version

--- routes/newitem/supportitem.js
+++ routes/newitem/supportitem.js
@@ -39,10 +39,16 @@ router.post('/test', function (req, res) {
                 var localFilePath = getLocalFilePath(scenes);
                 scenes.localCalibrationUrl = localFilePath.localCalibrationUrl;
                 scenes.localCameraUrl = localFilePath.localCameraUrl;
+
+                if (!fs.existsSync(scenes.localCalibrationUrl) || !fs.existsSync(scenes.localCameraUrl)) {
+                    console.log("文件不存在, 开始下一轮循环");
+                    return;
+                }
+
                 scenes.localCalibrationData = fs.readFileSync(scenes.localCalibrationUrl);
                 scenes.localCameraData = fs.readFileSync(scenes.localCameraUrl);
                 var pList = [];
-                supportList.forEach(function (support, index) {
+                supportList.forEach(function (support) {
                     pList.push(new Promise(function (resolve, reject) {
                         if (scenes.deviceid == support.deviceid) {
                             compareCalibrationFile(scenes, support, function (calibrationResult) {
@@ -57,9 +63,7 @@ router.post('/test', function (req, res) {
                         }
                     }));
                 });
-                //当 Promise 没有 resolve 操作时证明数据库没有存在对应的记录， 需要新增一条， 这个时候也要监控到
                 Promise.all(pList).then(function (resultList) {
-                    console.log("找到了至少一条 Support 记录");
                     var calibrationExist = false;
                     var cameraExist = false;
                     var oldSupport = {};
@@ -74,7 +78,6 @@ router.post('/test', function (req, res) {
                         }
                     });
                     if (!calibrationExist && !cameraExist) {
-                        console.log("两个文件均不相同");
                         var bucket = 'suteng';
                         var qiniu_key = 'calibration/' + scenes.key + '/' + 'calibration_2cam.xml';
                         var token = uptoken(bucket, qiniu_key);
@@ -84,28 +87,47 @@ router.post('/test', function (req, res) {
                             uploadFile(token, qiniu_key, scenes.localCameraUrl, function (cameraErr, cameraRet) {
                                 var calibrationCallback = JSON.parse(calibrationErr.error);
                                 var cameraCallback = JSON.parse(cameraErr.error);
-                                console.log(deploy.cdnPath + "/" + calibrationCallback.key);
-                                console.log(deploy.cdnPath + "/" + cameraCallback.key);
-                                //接下来进行写入数据库的操作
-                                //Nothing to do...
+                                var data = {
+                                    key: scenes.key,
+                                    deviceid: scenes.deviceid,
+                                    calibration_2cam_xml_url: deploy.cdnPath + "/" + calibrationCallback.key,
+                                    camera_xml_url: deploy.cdnPath + "/" + cameraCallback.key
+                                }
+                                db.Support.create(data).then(function (result) {
+                                    console.log("两个文件均不相同 新增数据成功");
+                                });
                             });
                         });
                     } else if (!calibrationExist || !cameraExist) {
-                        console.log("只有一个文件存在不相同");
                         var filePath = !calibrationExist ? scenes.localCalibrationUrl : scenes.localCameraUrl;
                         var fileName = !calibrationExist ? 'calibration_2cam.xml' : 'camera.xml';
                         var bucket = 'suteng';
                         var qiniu_key = 'calibration/' + scenes.key + '/' + fileName;
                         var token = uptoken(bucket, qiniu_key);
-                        uploadFile(token, qiniu_key, scenes.localCameraUrl, function (err, ret) {
+                        uploadFile(token, qiniu_key, filePath, function (err, ret) {
                             var callback = JSON.parse(err.error);
-                            console.log(deploy.cdnPath + "/" + callback.key);
-                            console.log(oldSupport);    //找到旧数据对象, 需要用三目运算符号判断哪个文件内容有变化, 另一个文件就无需上传, 直接加入url就好了
-                            //接下来进行写入数据库的操作
-                            //Nothing to do...
+                            var data = {};
+                            if (!calibrationExist) {
+                                data = {
+                                    key: scenes.key,
+                                    deviceid: scenes.deviceid,
+                                    calibration_2cam_xml_url: deploy.cdnPath + "/" + callback.key,
+                                    camera_xml_url: oldSupport.camera_xml_url
+                                }
+                            } else {
+                                data = {
+                                    key: scenes.key,
+                                    deviceid: scenes.deviceid,
+                                    calibration_2cam_xml_url: oldSupport.calibration_2cam_xml_url,
+                                    camera_xml_url: deploy.cdnPath + "/" + callback.key
+                                }
+                            }
+                            db.Support.create(data).then(function (result) {
+                                console.log("只有一个文件存在不相同 新增数据成功");
+                            });
                         });
                     } else {
-                        console.log("两个文件均相同");
+                        console.log("两个文件均相同, 不执行任何操作");
                     }
                 });
             });
@@ -127,9 +149,9 @@ var compareCalibrationFile = function (scenes, support, callback) {
     readStream.pipe(writeStream);
     writeStream.on("finish", function () {
         var downCalibrationData = fs.readFileSync(writeStream.path);
-        if (fs.existsSync(writeStream.path)) {
-            //fs.unlinkSync(writeStream.path);
-        }
+        // if (fs.existsSync(writeStream.path)) {
+        //     fs.unlinkSync(writeStream.path);
+        // }
         scenes.localCalibrationData.toString() == downCalibrationData.toString() ? callback(true) : callback(false);
     });
 }
@@ -141,9 +163,9 @@ var compareCameraFile = function (scenes, support, callback) {
     readStream.pipe(writeStream);
     writeStream.on("finish", function () {
         var downCameraData = fs.readFileSync(writeStream.path);
-        if (fs.existsSync(writeStream.path)) {
-            //fs.unlinkSync(writeStream.path);
-        }
+        // if (fs.existsSync(writeStream.path)) {
+        //     fs.unlinkSync(writeStream.path);
+        // }
         scenes.localCameraData.toString() == downCameraData.toString() ? callback(true) : callback(false);
     });
 }
