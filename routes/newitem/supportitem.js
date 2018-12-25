937588d134ba5ab8c5c1d837aca33860209cbb4f
roger <280135694@qq.com>
2018-12-25 11:10:38 +0800

    Update latest dev version, ready to go online

--- routes/newitem/supportitem.js
+++ routes/newitem/supportitem.js
@@ -34,147 +34,177 @@ router.post('/test', function (req, res) {
         db.Scenes.sequelize.query('select * from support order by deviceid asc', {
             model: db.Support
         }).then(function (supportList) {
-            scenesList.forEach(function (scenes, index) {
-                var localFilePath = getLocalFilePath(scenes);
-                scenes.localCalibrationUrl = localFilePath.localCalibrationUrl;
-                scenes.localCameraUrl = localFilePath.localCameraUrl;
-
-                if (!fs.existsSync(scenes.localCalibrationUrl) || !fs.existsSync(scenes.localCameraUrl)) {
-                    //console.log("文件不存在, 开始下一轮循环: " + index);
-                    return;
-                }
-
-                scenes.localCalibrationData = fs.readFileSync(scenes.localCalibrationUrl);
-                scenes.localCameraData = fs.readFileSync(scenes.localCameraUrl);
-                var pList = [];
-                var count = 0;
-                supportList.forEach(function (support, index) {
-                    if (scenes.deviceid == support.deviceid) {
-                        pList.push(new Promise(function (resolve, reject) {
-                            count++;
-                            compareCalibrationFile(scenes, support, function (calibrationResult) {
-                                compareCameraFile(scenes, support, function (cameraResult) {
-                                    resolve({
-                                        calibrationResult: calibrationResult,
-                                        cameraResult: cameraResult,
-                                        support: support
-                                    });
-                                });
-                            });
-                        }));
-                    }
-                });
-                if (count == 0) {
-                    console.log("没有发现文件");
-                    pList.push(new Promise(function (resolve, reject) {
-                        resolve({
-                            calibrationResult: false,
-                            cameraResult: false,
-                            support: {}
-                        });
-                    }));
-                }
-                Promise.all(pList).then(function (resultList) {
-                    //终止程序-------------------------------------------------------------------------------------------
-                    return;
-                    var calibrationExist = false;
-                    var cameraExist = false;
-                    var oldSupport = {};
-                    resultList.forEach(function (result) {
-                        if (result.calibrationResult) {
-                            calibrationExist = true;
-                            oldSupport = result.support;
-                        }
-                        if (result.cameraResult) {
-                            cameraExist = true;
-                            oldSupport = result.support;
-                        }
-                    });
-                    if (!calibrationExist && !cameraExist) {
-                        var bucket = 'suteng';
-                        var qiniu_key = 'calibration/' + scenes.key + '/' + 'calibration_2cam.xml';
-                        var token = uptoken(bucket, qiniu_key);
-                        uploadFile(token, qiniu_key, scenes.localCalibrationUrl, function (calibrationErr, calibrationRet) {
-                            qiniu_key = 'calibration/' + scenes.key + '/' + 'camera.xml';
-                            token = uptoken(bucket, qiniu_key);
-                            uploadFile(token, qiniu_key, scenes.localCameraUrl, function (cameraErr, cameraRet) {
-                                var calibrationCallback = JSON.parse(calibrationErr.error);
-                                var cameraCallback = JSON.parse(cameraErr.error);
-                                var data = {
-                                    key: scenes.key,
-                                    deviceid: scenes.deviceid,
-                                    calibration_2cam_xml_url: deploy.cdnPath + "/" + calibrationCallback.key,
-                                    camera_xml_url: deploy.cdnPath + "/" + cameraCallback.key
-                                }
-                                db.Support.create(data).then(function (result) {
-                                    console.log("两个文件均不相同, 新增数据成功");
-                                });
-                            });
-                        });
-                    } else if (!calibrationExist || !cameraExist) {
-                        var filePath = !calibrationExist ? scenes.localCalibrationUrl : scenes.localCameraUrl;
-                        var fileName = !calibrationExist ? 'calibration_2cam.xml' : 'camera.xml';
-                        var bucket = 'suteng';
-                        var qiniu_key = 'calibration/' + scenes.key + '/' + fileName;
-                        var token = uptoken(bucket, qiniu_key);
-                        uploadFile(token, qiniu_key, filePath, function (err, ret) {
-                            var callback = JSON.parse(err.error);
-                            var data = {};
-                            if (!calibrationExist) {
-                                data = {
-                                    key: scenes.key,
-                                    deviceid: scenes.deviceid,
-                                    calibration_2cam_xml_url: deploy.cdnPath + "/" + callback.key,
-                                    camera_xml_url: oldSupport.camera_xml_url
-                                }
-                            } else {
-                                data = {
-                                    key: scenes.key,
-                                    deviceid: scenes.deviceid,
-                                    calibration_2cam_xml_url: oldSupport.calibration_2cam_xml_url,
-                                    camera_xml_url: deploy.cdnPath + "/" + callback.key
-                                }
-                            }
-                            db.Support.create(data).then(function (result) {
-                                console.log("只有一个文件存在不相同, 新增数据成功");
-                            });
-                        });
-                    } else {
-                        console.log("两个文件均相同, 不执行任何操作");
-                    }
-                });
+            downloadAllFile(supportList, function (resultList) {
+                executeCompare(scenesList, resultList);
             });
         });
     });
 });
 
 
+var downloadAllFile = function (supportList, callback) {
+    var tempFilePath = __dirname + '/temp_file/';
+    if (fs.existsSync(tempFilePath)) {
+        var files = fs.readdirSync(tempFilePath);
+        files.forEach(function (file, index) {
+            fs.unlinkSync(tempFilePath + file);
+        });
+        fs.rmdirSync(tempFilePath);
+        fs.mkdirSync(tempFilePath);
+    } else {
+        fs.mkdirSync(tempFilePath);
+    }
+
+    //清除本地 temp_file 文件夹开关, 只需要取消 return 的注释即可
+    //return;
+
+    var pList = [];
+    supportList.forEach(function (support, index) {
+        pList.push(new Promise(function (resolve, reject) {
+            var tempCalibrationPath = tempFilePath + support.deviceid + '_' + support.key + '_calibration_2cam.xml';
+            var tempCameraPath = tempFilePath + support.deviceid + '_' + support.key + '_camera.xml';
+            request(support.calibration_2cam_xml_url).pipe(fs.createWriteStream(tempCalibrationPath)).on("finish", function () {
+                request(support.camera_xml_url).pipe(fs.createWriteStream(tempCameraPath)).on("finish", function () {
+                    support.downCalibrationPath = tempCalibrationPath;
+                    support.downCameraPath = tempCameraPath;
+                    resolve(support);
+                });
+            });
+        }));
+    });
+    Promise.all(pList).then(function (resultList) {
+        callback(resultList);
+    });
+}
+
+
+var executeCompare = function (scenesList, supportList) {
+    scenesList.forEach(function (scenes, index) {
+        var localFilePath = getLocalFilePath(scenes);
+        scenes.localCalibrationPath = localFilePath.localCalibrationPath;
+        scenes.localCameraPath = localFilePath.localCameraPath;
+
+        if (!fs.existsSync(scenes.localCalibrationPath) || !fs.existsSync(scenes.localCameraPath)) {
+            return;
+        }
+
+        scenes.localCalibrationFile = fs.readFileSync(scenes.localCalibrationPath);
+        scenes.localCameraFile = fs.readFileSync(scenes.localCameraPath);
+        var pList = [];
+        var count = 0;
+        supportList.forEach(function (support, index) {
+            if (scenes.deviceid == support.deviceid) {
+                pList.push(new Promise(function (resolve, reject) {
+                    count++;
+                    compareCalibrationFile(scenes, support, function (calibrationResult) {
+                        compareCameraFile(scenes, support, function (cameraResult) {
+                            resolve({
+                                calibrationResult: calibrationResult,
+                                cameraResult: cameraResult,
+                                support: support
+                            });
+                        });
+                    });
+                }));
+            }
+        });
+        if (count == 0) {
+            console.log("没有发现文件");
+            pList.push(new Promise(function (resolve, reject) {
+                resolve({
+                    calibrationResult: false,
+                    cameraResult: false,
+                    support: {}
+                });
+            }));
+        }
+        Promise.all(pList).then(function (resultList) {
+            var calibrationExist = false;
+            var cameraExist = false;
+            var oldSupport = {};
+            resultList.forEach(function (result) {
+                if (result.calibrationResult) {
+                    calibrationExist = true;
+                    oldSupport = result.support;
+                }
+                if (result.cameraResult) {
+                    cameraExist = true;
+                    oldSupport = result.support;
+                }
+            });
+            if (!calibrationExist && !cameraExist) {
+                var bucket = 'suteng';
+                var qiniu_key = 'calibration/' + scenes.key + '/' + 'calibration_2cam.xml';
+                var token = uptoken(bucket, qiniu_key);
+                uploadFile(token, qiniu_key, scenes.localCalibrationPath, function (calibrationErr, calibrationRet) {
+                    qiniu_key = 'calibration/' + scenes.key + '/' + 'camera.xml';
+                    token = uptoken(bucket, qiniu_key);
+                    uploadFile(token, qiniu_key, scenes.localCameraPath, function (cameraErr, cameraRet) {
+                        var calibrationCallback = JSON.parse(calibrationErr.error);
+                        var cameraCallback = JSON.parse(cameraErr.error);
+                        var data = {
+                            key: scenes.key,
+                            deviceid: scenes.deviceid,
+                            calibration_2cam_xml_url: deploy.cdnPath + "/" + calibrationCallback.key,
+                            camera_xml_url: deploy.cdnPath + "/" + cameraCallback.key
+                        }
+                        db.Support.create(data).then(function (result) {
+                            console.log("两个文件均不相同, 新增数据成功");
+                        });
+                    });
+                });
+            } else if (!calibrationExist || !cameraExist) {
+                var filePath = !calibrationExist ? scenes.localCalibrationPath : scenes.localCameraPath;
+                var fileName = !calibrationExist ? 'calibration_2cam.xml' : 'camera.xml';
+                var bucket = 'suteng';
+                var qiniu_key = 'calibration/' + scenes.key + '/' + fileName;
+                var token = uptoken(bucket, qiniu_key);
+                uploadFile(token, qiniu_key, filePath, function (err, ret) {
+                    var callback = JSON.parse(err.error);
+                    var data = {};
+                    if (!calibrationExist) {
+                        data = {
+                            key: scenes.key,
+                            deviceid: scenes.deviceid,
+                            calibration_2cam_xml_url: deploy.cdnPath + "/" + callback.key,
+                            camera_xml_url: oldSupport.camera_xml_url
+                        }
+                    } else {
+                        data = {
+                            key: scenes.key,
+                            deviceid: scenes.deviceid,
+                            calibration_2cam_xml_url: oldSupport.calibration_2cam_xml_url,
+                            camera_xml_url: deploy.cdnPath + "/" + callback.key
+                        }
+                    }
+                    db.Support.create(data).then(function (result) {
+                        console.log("只有一个文件存在不相同, 新增数据成功");
+                    });
+                });
+            } else {
+                console.log("两个文件均相同, 不执行任何操作");
+            }
+        });
+    });
+}
+
+
 var getLocalFilePath = function (scenes) {
-    var localCalibrationUrl = path.join(__dirname, '../../public/pano/pano2T/' + scenes.key + '/calibration_2cam.xml');
-    var localCameraUrl = path.join(__dirname, '../../public/pano/pano2T/' + scenes.key + '/camera.xml');
-    return {localCalibrationUrl: localCalibrationUrl, localCameraUrl: localCameraUrl};
+    var localCalibrationPath = path.join(__dirname, '../../public/pano/pano2T/' + scenes.key + '/calibration_2cam.xml');
+    var localCameraPath = path.join(__dirname, '../../public/pano/pano2T/' + scenes.key + '/camera.xml');
+    return {localCalibrationPath: localCalibrationPath, localCameraPath: localCameraPath};
 }
 
 
 var compareCalibrationFile = function (scenes, support, callback) {
-    var readStream = request(support.calibration_2cam_xml_url);
-    var writeStream = fs.createWriteStream(__dirname + '/calibration_2cam.xml');
-    readStream.pipe(writeStream);
-    writeStream.on("finish", function () {
-        var downCalibrationData = fs.readFileSync(writeStream.path);
-        scenes.localCalibrationData.toString() == downCalibrationData.toString() ? callback(true) : callback(false);
-    });
+    var downCalibrationFile = fs.readFileSync(support.downCalibrationPath);
+    scenes.localCalibrationFile.toString() == downCalibrationFile.toString() ? callback(true) : callback(false);
 }
 
 
 var compareCameraFile = function (scenes, support, callback) {
-    var readStream = request(support.camera_xml_url);
-    var writeStream = fs.createWriteStream(__dirname + '/camera.xml');
-    readStream.pipe(writeStream);
-    writeStream.on("finish", function () {
-        var downCameraData = fs.readFileSync(writeStream.path);
-        scenes.localCameraData.toString() == downCameraData.toString() ? callback(true) : callback(false);
-    });
+    var downCameraFile = fs.readFileSync(support.downCameraPath);
+    scenes.localCameraFile.toString() == downCameraFile.toString() ? callback(true) : callback(false);
 }
 
 
